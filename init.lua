4a0aa46b59ea892f793eb55f5ac95bfa9553256e
Borna Kreso <borna.kreso@inthing.io>
2024-04-18 13:59:10 +0200

    Add cmp-buffer

--- init.lua
+++ init.lua
@@ -78,6 +78,7 @@ require('lazy').setup({
       -- Adds LSP completion capabilities
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
 
       -- Adds a number of user-friendly snippets
       'rafamadriz/friendly-snippets',
@@ -656,6 +657,7 @@ cmp.setup {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
     { name = 'path' },
+    { name = 'buffer' },
   },
 }
 
